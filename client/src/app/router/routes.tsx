be258b391050c0e63b5938fc2276368655041152
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-04 18:48:00 +0530

    feat: setup auth layout for auth related routes

--- client/src/app/router/routes.tsx
+++ client/src/app/router/routes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import RegisterPage from "@/features/auth/pages/RegisterPage";
+import AuthLayout from "@/app/layouts/AuthLayout";
 
 export const appRoutes = (
   <Routes>
-    <Route path="/login" element={<LoginPage />} />
+    <Route element={<AuthLayout />}>
+      <Route path="/login" element={<LoginPage />} />
 
-    <Route path="/register" element={<RegisterPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Route>
   </Routes>
 );

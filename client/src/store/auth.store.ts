12d8acd23bbb7ba4e42e6af5455d2d1995d64832
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-08 11:43:23 +0530

    feat: ass user auth store

---
+++ client/src/store/auth.store.ts
@@ -0,0 +1,97 @@
+import { create } from "zustand";
+import { loginApi } from "@/features/auth/api/login";
+import { registerApi } from "@/features/auth/api/register";
+import { getCurrentUserApi } from "@/features/auth/api/get-current-user";
+import type { User } from "@/features/auth/types/auth.types";
+
+interface AuthStore {
+  user: User | null;
+  token: string | null;
+  loading: boolean;
+
+  login: (email: string, password: string) => Promise<void>;
+  register: (
+    fullName: string,
+    email: string,
+    password: string,
+  ) => Promise<void>;
+
+  initialize: () => Promise<void>;
+
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  token: localStorage.getItem("token"),
+  loading: false,
+
+  login: async (email, password) => {
+    set({ loading: true });
+
+    try {
+      const response = await loginApi(email, password);
+
+      localStorage.setItem("token", response.data.token);
+
+      set({
+        user: response.data.user,
+        token: response.data.token,
+        loading: false,
+      });
+    } catch (error) {
+      set({ loading: false });
+      throw error;
+    }
+  },
+
+  register: async (fullName, email, password) => {
+    set({ loading: true });
+
+    try {
+      const response = await registerApi(fullName, email, password);
+
+      localStorage.setItem("token", response.data.token);
+
+      set({
+        user: response.data.user,
+        token: response.data.token,
+        loading: false,
+      });
+    } catch (error) {
+      set({ loading: false });
+      throw error;
+    }
+  },
+
+  initialize: async () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) return;
+
+    try {
+      const response = await getCurrentUserApi();
+
+      set({
+        user: response.data,
+        token,
+      });
+    } catch {
+      localStorage.removeItem("token");
+
+      set({
+        user: null,
+        token: null,
+      });
+    }
+  },
+
+  logout: () => {
+    localStorage.removeItem("token");
+
+    set({
+      user: null,
+      token: null,
+    });
+  },
+}));

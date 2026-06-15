bf5bbf9c4b569cd22bd234c42b525cb33e2d8b6e
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-15 17:05:13 +0530

    feat: add corner arrow to the tooltip for different positions

--- client/src/shared/components/ui/Tooltip.tsx
+++ client/src/shared/components/ui/Tooltip.tsx
@@ -1,19 +1,62 @@
 import { useState, type ReactNode } from "react";
 
+type TooltipPosition = "top" | "bottom" | "left" | "right";
 interface TooltipProp {
   content: string;
   children: ReactNode;
-  position?: "top" | "bottom" | "left" | "right";
+  position?: TooltipPosition;
 }
 
 function Tooltip({ content, children, position = "top" }: TooltipProp) {
   const [isVisible, setIsVisible] = useState(false);
 
-  const positionClasses = {
+  const positionClasses: Record<TooltipPosition, string> = {
     top: "bottom-full left-1/2 -translate-x-1/2 mb-2",
     bottom: "top-full left-1/2 -translate-x-1/2 mt-2",
     left: "right-full top-1/2 -translate-y-1/2 mr-2",
     right: "left-full top-1/2 -translate-y-1/2 ml-2",
+  };
+  const arrowClasses: Record<TooltipPosition, string> = {
+    top: `
+    absolute
+    top-[85%]
+    left-1/2
+    -translate-x-1/2
+    border-l-4 border-r-4 border-t-4
+    border-l-transparent
+    border-r-transparent
+    border-t-black
+  `,
+    bottom: `
+    absolute
+    bottom-[85%]
+    left-1/2
+    -translate-x-1/2
+    border-l-4 border-r-4 border-b-4
+    border-l-transparent
+    border-r-transparent
+    border-b-black
+  `,
+    left: `
+    absolute
+    left-[85%]
+    top-1/2
+    -translate-y-1/2
+    border-t-4 border-b-4 border-l-4
+    border-t-transparent
+    border-b-transparent
+    border-l-black
+  `,
+    right: `
+    absolute
+    right-[95%]
+    top-1/2
+    -translate-y-1/2
+    border-t-4 border-b-4 border-r-4
+    border-t-transparent
+    border-b-transparent
+    border-r-black
+  `,
   };
   return (
     <div
@@ -37,6 +80,16 @@ function Tooltip({ content, children, position = "top" }: TooltipProp) {
             ${positionClasses[position]}`}
         >
           {content}
+          {/* Arrow */}
+          <div
+            className={`
+              h-2
+              w-2
+              rotate-45
+              bg-black
+              ${arrowClasses[position]}
+            `}
+          />
         </div>
       )}
     </div>

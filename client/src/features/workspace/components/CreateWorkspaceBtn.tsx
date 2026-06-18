1c397ecb876565366a0f56e472b0a8c4e1747a8e
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-19 00:29:18 +0530

    fix: adjust layout of Create Workspace button for improved visual hierarchy

--- client/src/features/workspace/components/CreateWorkspaceBtn.tsx
+++ client/src/features/workspace/components/CreateWorkspaceBtn.tsx
@@ -15,9 +15,16 @@ function CreateWorkspaceBtn() {
     <>
       <div
         onClick={() => setIsModalOpen(true)}
-        className="border bg-bg-sidebar cursor-pointer border-border-default rounded-lg w-62 flex items-center justify-center hover:bg-base-hover h-56"
+        className="border bg-bg-sidebar cursor-pointer border-border-default rounded-lg w-62 flex flex-col items-center justify-center hover:bg-base-hover h-56"
       >
         <Plus className="size-5" />
+        <div className="text-center w-3/4">
+          <p className="font-medium text-text-primary">Create Workspace</p>
+
+          <p className="text-xs text-text-secondary">
+            Create a workspace to manage your projects
+          </p>
+        </div>
       </div>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <CreateWorkspace

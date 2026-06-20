4734373721cb8fdfee8ea9a8ebb198d3a503252a
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-20 16:30:34 +0530

    feat: enhance WorkspaceStore with workspaces management and setWorkspaces method

--- client/src/store/workspace.store.ts
+++ client/src/store/workspace.store.ts
@@ -1,9 +1,14 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import type { WorkspaceProp } from "@/features/workspace/types/workspace.type";
 
 interface WorkspaceStore {
+  workspaces: WorkspaceProp[];
+
   currentWorkspaceId: string | null;
   currentProjectId: string | null;
+
+  setWorkspaces: (workspaces: WorkspaceProp[]) => void;
 
   setCurrentWorkspaceId: (id: string) => void;
   setCurrentProjectId: (id: string) => void;
@@ -14,8 +19,15 @@ interface WorkspaceStore {
 export const useWorkspaceStore = create<WorkspaceStore>()(
   persist(
     (set) => ({
+      workspaces: [],
+
       currentWorkspaceId: null,
       currentProjectId: null,
+
+      setWorkspaces: (workspaces) =>
+        set({
+          workspaces,
+        }),
 
       setCurrentWorkspaceId: (id) =>
         set({

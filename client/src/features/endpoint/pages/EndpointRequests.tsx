046b0edc447770ecb8c8812317cf31e5df8b1f89
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-20 19:07:14 +0530

    feat: integrate useCurrentEndpoint and useLiveRequests hooks into EndpointRequests component

--- client/src/features/endpoint/pages/EndpointRequests.tsx
+++ client/src/features/endpoint/pages/EndpointRequests.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import { getRequests } from "@/features/request/api/getRequests";
 import type { RequestLog } from "@/features/request/types/request.types";
 import RequestCard from "@/features/request/components/RequestCard";
+import { useCurrentEndpoint } from "../hooks/useCurrentEndpoint";
+import { useLiveRequests } from "@/features/request/hook/useLiveRequests";
 
 function EndpointRequests() {
   const { endpointId } = useParams();
-
+  const endpoint = useCurrentEndpoint();
   const [requests, setRequests] = useState<RequestLog[]>([]);
 
+  useLiveRequests(endpoint?.slug);
   useEffect(() => {
     if (!endpointId) return;
 

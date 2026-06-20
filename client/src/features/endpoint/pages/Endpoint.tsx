d101dd248cf07d0df0ceba527e35c1c46c66269f
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-20 17:27:16 +0530

    feat: implement endpoint fetching logic in Endpoint component

--- client/src/features/endpoint/pages/Endpoint.tsx
+++ client/src/features/endpoint/pages/Endpoint.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { getEndpoints } from "../api/getEndpoints";
+import type { Endpoint as EndpointType } from "../types/endpoint.types";
 
 function Endpoint() {
-  return <div>Endpoint</div>;
+  const { projectId } = useParams();
+
+  const [endpoints, setEndpoints] = useState<EndpointType[]>([]);
+
+  useEffect(() => {
+    if (!projectId) return;
+
+    const fetchEndpoints = async () => {
+      const data = await getEndpoints(projectId);
+
+      setEndpoints(data);
+    };
+
+    fetchEndpoints();
+  }, [projectId]);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(endpoints, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default Endpoint;

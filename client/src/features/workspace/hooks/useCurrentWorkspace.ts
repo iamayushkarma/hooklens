6cbd5af592784742383efec13ba07f5761da91a6
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-20 16:32:37 +0530

    feat: refactor useCurrentWorkspace hook to include currentWorkspace in return value

--- client/src/features/workspace/hooks/useCurrentWorkspace.ts
+++ client/src/features/workspace/hooks/useCurrentWorkspace.ts
@@ -1,11 +1,18 @@
 import { useWorkspaceStore } from "@/store/workspace.store";
 
 export function useCurrentWorkspace() {
+  const workspaces = useWorkspaceStore((state) => state.workspaces);
+
   const currentWorkspaceId = useWorkspaceStore(
     (state) => state.currentWorkspaceId,
   );
 
+  const currentWorkspace = workspaces.find(
+    (workspace) => workspace._id === currentWorkspaceId,
+  );
+
   return {
+    currentWorkspace,
     currentWorkspaceId,
   };
 }

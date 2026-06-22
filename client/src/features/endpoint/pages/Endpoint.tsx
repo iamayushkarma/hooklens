090291ec37daf6dc526609a70a876dc2a5ecc8b4
Ayush Karma <ayushkarma.dev@gmail.com>
2026-06-22 14:06:27 +0530

    feat: enhance Endpoint component with create endpoint functionality and layout improvements

--- client/src/features/endpoint/pages/Endpoint.tsx
+++ client/src/features/endpoint/pages/Endpoint.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import CreateEndpoint from "../components/CreateEndpoint";
+import CreateEndpointBtn from "../components/CreateEndpointBtn";
 import { getEndpoints } from "../api/getEndpoints";
 import type { Endpoint as EndpointType } from "../types/endpoint.types";
 import EndpointCard from "../components/EndpointCard";
 
 function Endpoint() {
   const { projectId } = useParams();
-
+  const [showCreate, setShowCreate] = useState(false);
   const [endpoints, setEndpoints] = useState<EndpointType[]>([]);
 
   useEffect(() => {
@@ -21,10 +22,16 @@ function Endpoint() {
 
     fetchEndpoints();
   }, [projectId]);
-
   return (
-    <div>
-      {/* <pre>{JSON.stringify(endpoints, null, 2)}</pre> */}
+    <div className="space-y-6">
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-semibold">Endpoints</h2>
+
+        <CreateEndpointBtn onClick={() => setShowCreate(true)} />
+      </div>
+
+      {showCreate && <CreateEndpoint onClose={() => setShowCreate(false)} />}
+
       <div className="space-y-4">
         {endpoints.map((endpoint) => (
           <EndpointCard key={endpoint._id} endpoint={endpoint} />

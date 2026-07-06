02fdbac698791df4269be401482b4abd1b6f3881
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-06 18:24:21 +0530

    feat: add Notification interface to define notification structure

---
+++ client/src/features/notification/types/notification.types.ts
@@ -0,0 +1,27 @@
+export interface Notification {
+  _id: string;
+
+  type:
+    | "workspace_invite"
+    | "member_joined"
+    | "member_left"
+    | "role_changed"
+    | "project_created"
+    | "endpoint_created";
+
+  title: string;
+
+  message: string;
+
+  actionRequired: boolean;
+
+  isRead: boolean;
+
+  createdAt: string;
+
+  data: {
+    invitationId?: string;
+    workspaceId?: string;
+    role?: string;
+  };
+}

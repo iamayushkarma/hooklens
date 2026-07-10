6627432278bf8ad3e0375f3543b116309d6132e5
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-10 18:24:45 +0530

    feat: improve loading state handling in Projects component

--- client/src/features/project/pages/Projects.tsx
+++ client/src/features/project/pages/Projects.tsx
@@ -1,32 +1,36 @@
 import { useEffect, useState } from "react";
-
 import { useWorkspaceStore } from "@/store/workspace.store";
-
 import { getProjects } from "../api/getProject";
 import CreateProjectCard from "../components/CreateProjectBtn";
 import { ProjectCard } from "../components/ProjectCard";
-
 import type { Project } from "../types/project.types";
+import { ProjectsPageSkeleton } from "@/shared/components/skletons/ProjectsPageSkeleton";
 
 function Projects() {
   const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const workspaceId = useWorkspaceStore((state) => state.currentWorkspaceId);
-
   const setProjectsStore = useWorkspaceStore((state) => state.setProjects);
 
   const fetchProjects = async () => {
     if (!workspaceId) return;
-
-    const res = await getProjects(workspaceId);
-
-    setProjects(res.projects);
-    setProjectsStore(res.projects);
+    try {
+      const res = await getProjects(workspaceId);
+      setProjects(res.projects);
+      setProjectsStore(res.projects);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchProjects();
   }, [workspaceId]);
+
+  if (loading) {
+    return <ProjectsPageSkeleton />;
+  }
 
   return (
     <div>
@@ -38,7 +42,6 @@ function Projects() {
             onSuccess={fetchProjects}
           />
         ))}
-
         <CreateProjectCard onSuccess={fetchProjects} />
       </div>
     </div>

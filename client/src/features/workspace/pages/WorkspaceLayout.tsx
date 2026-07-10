ef2ff631e320242c5a0c0f864ebaca1e58269f5c
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-10 18:27:01 +0530

    refactor: streamline WorkspaceLayout component structure and improve readability

--- client/src/features/workspace/pages/WorkspaceLayout.tsx
+++ client/src/features/workspace/pages/WorkspaceLayout.tsx
@@ -1,17 +1,15 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useCurrentWorkspace } from "../hooks/useCurrentWorkspace";
 import { RoleBadge } from "@/shared/components/ui/role-badge";
 import { FolderKanban, UsersRound } from "lucide-react";
 import Tabs from "@/shared/components/ui/Tabs";
+import { WorkspaceHeaderSkeleton } from "@/shared/components/skletons/WorkspaceLayoutSkeleton";
 
 function WorkspaceLayout() {
   const { currentWorkspace, currentWorkspaceId } = useCurrentWorkspace();
 
   const tabs = [
-    {
-      label: "Projects",
-      path: `/dashboard/workspaces/${currentWorkspaceId}`,
-    },
+    { label: "Projects", path: `/dashboard/workspaces/${currentWorkspaceId}` },
     {
       label: "Members",
       path: `/dashboard/workspaces/${currentWorkspaceId}/members`,
@@ -24,27 +22,26 @@ function WorkspaceLayout() {
 
   return (
     <div className="space-y-6">
-      {/* Header */}
-      <div className="pb-4">
-        <h1 className="text-2xl font-semibold">{currentWorkspace?.name}</h1>
-
-        <div className="mt-4 flex items-center gap-5 text-sm text-text-secondary">
-          <RoleBadge role={currentWorkspace?.yourRole!} />
-
-          <div className="flex items-center justify-center gap-1.5">
-            <UsersRound className="size-4" />
-            <span>{currentWorkspace?.memberCount} Members</span>
-          </div>
-
-          <div className="flex items-center justify-center gap-1.5">
-            <FolderKanban className="size-4" />
-            <span>{currentWorkspace?.projectCount} Projects</span>
+      {currentWorkspace ? (
+        <div className="pb-4">
+          <h1 className="text-2xl font-semibold">{currentWorkspace.name}</h1>
+          <div className="mt-4 flex items-center gap-5 text-sm text-text-secondary">
+            <RoleBadge role={currentWorkspace.yourRole} />
+            <div className="flex items-center justify-center gap-1.5">
+              <UsersRound className="size-4" />
+              <span>{currentWorkspace.memberCount} Members</span>
+            </div>
+            <div className="flex items-center justify-center gap-1.5">
+              <FolderKanban className="size-4" />
+              <span>{currentWorkspace.projectCount} Projects</span>
+            </div>
           </div>
         </div>
-      </div>
-      {/* Navigation */}
+      ) : (
+        <WorkspaceHeaderSkeleton />
+      )}
+
       <Tabs tabs={tabs} />
-      {/* Page Content */}
       <Outlet />
     </div>
   );

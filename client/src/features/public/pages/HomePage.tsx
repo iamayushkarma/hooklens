8a5a325b5b535c9e86c0061102afaa37e0770ec9
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-12 16:37:57 +0530

    feat: add HeroDashboardImage component to HomePage for enhanced visual appeal

--- client/src/features/public/pages/HomePage.tsx
+++ client/src/features/public/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import HeroDashboardImage from "../section/HeroDashboardImage";
 import HeroSection from "../section/HeroSection";
 
 function HomePage() {
   return (
     <div className="top-16">
       <HeroSection />
+      <HeroDashboardImage />
     </div>
   );
 }

69a2c1d0184d56a90367349b2802e7ef5bf368f7
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-13 16:31:54 +0530

    fix: replace duplicate CollaborationCard with PublishStackCard in BentoSection

--- client/src/features/public/section/Bento/BentoSection.tsx
+++ client/src/features/public/section/Bento/BentoSection.tsx
@@ -1,10 +1,11 @@
 import CollaborationCard from "./cards/CollaborationCard";
+import PublishStackCard from "./cards/PublishStackCard";
 
 function BentoSection() {
   return (
     <div className="w-6xl grid grid-cols-3 gap-8 bg-white">
       <CollaborationCard />
-      <CollaborationCard />
+      <PublishStackCard />
       <CollaborationCard />
     </div>
   );

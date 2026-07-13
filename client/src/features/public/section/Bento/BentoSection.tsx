8cf042519c7ea995f767f1f96f70c628a6d57846
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-13 19:30:25 +0530

    fix: replace duplicate CollaborationCard with WriteBetterCard in BentoSection

--- client/src/features/public/section/Bento/BentoSection.tsx
+++ client/src/features/public/section/Bento/BentoSection.tsx
@@ -1,12 +1,13 @@
 import CollaborationCard from "./cards/CollaborationCard";
 import WebhookCaptureCard from "./cards/WebhookCaptureCard";
+import WriteBetterCard from "./cards/WriteBetterCard";
 
 function BentoSection() {
   return (
     <div className="w-6xl grid grid-cols-3 gap-8 bg-white">
       <CollaborationCard />
       <WebhookCaptureCard />
-      <CollaborationCard />
+      <WriteBetterCard />
     </div>
   );
 }

0d0159e55f78f20a9a6584a2cbc2e4429b561397
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-17 14:40:57 +0530

    feat: add ThemeSwitcher component to HomePage for improved user experience

--- client/src/features/public/pages/HomePage.tsx
+++ client/src/features/public/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import ThemeSwitcher from "@/shared/components/ui/ThemeToggler";
 import BentoSection from "../section/Bento/BentoSection";
 import HeroDashboardImage from "../section/HeroDashboardImage";
 import HeroSection from "../section/HeroSection";
@@ -5,6 +6,7 @@ import HeroSection from "../section/HeroSection";
 function HomePage() {
   return (
     <div className="top-16">
+      <ThemeSwitcher />
       <HeroSection />
       <HeroDashboardImage />
       <div className="flex items-center justify-center bg-white">

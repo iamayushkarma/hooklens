8e3bd3ed291d0be257742713a73ec18f2092e955
Ayush Karma <ayushkarma.dev@gmail.com>
2026-07-17 15:50:26 +0530

    feat: enhance CardLayout component with additional props for improved functionality and styling

--- client/src/features/public/section/Bento/CardLayout.tsx
+++ client/src/features/public/section/Bento/CardLayout.tsx
@@ -1,7 +1,45 @@
 import { type ReactNode } from "react";
+import { motion, type Transition } from "motion/react";
 
-function CardLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+interface CardLayoutProp {
+  children: ReactNode;
+  heading: string;
+  subHeading: string;
+  className?: string;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+  onHoverStart?: () => void;
+  onHoverEnd?: () => void;
+  transition?: Transition;
+}
+
+function CardLayout({
+  className,
+  heading,
+  children,
+  subHeading,
+  onMouseEnter,
+  onMouseLeave,
+  onHoverStart,
+  onHoverEnd,
+  transition,
+}: CardLayoutProp) {
+  return (
+    <motion.div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onHoverStart={onHoverStart}
+      onHoverEnd={onHoverEnd}
+      transition={transition}
+      className={`${className} relative h-82 group bg-[#fafafa] border border-border-default rounded-3xl overflow-hidden  cursor-default p-6`}
+    >
+      {children}
+      <div className="relative">
+        <h3 className="text-text-primary font-semibold">{heading}</h3>
+        <p className="text-text-secondary">{subHeading}</p>
+      </div>
+    </motion.div>
+  );
 }
 
 export default CardLayout;

406d0836b0d79c483c6b151d57e049612ee6b2ea
Ayush Karma <ayushkarma.dev@gmail.com>
2026-08-11 16:06:01 +0530

    style: add online status check and offline page rendering in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import AppRouter from "./app/router/AppRouter";
 import { useAuthStore } from "./store/auth.store";
+import useOnlineStatus from "./shared/hooks/useOnlineStatus";
+import OfflinePage from "./features/public/pages/OfflinePage";
 
 const App = () => {
   const { initialize, initialized } = useAuthStore();
@@ -9,6 +11,9 @@ const App = () => {
     initialize();
   }, []);
 
+  const isOnline = useOnlineStatus();
+
+  if (!isOnline) return <OfflinePage />;
   if (!initialized) return;
   return <AppRouter />;
 };

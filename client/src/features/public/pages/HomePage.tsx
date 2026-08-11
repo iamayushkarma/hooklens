7ac57dbda3c40a1eb15d07ce2c8079753294071d
Ayush Karma <ayushkarma.dev@gmail.com>
2026-08-11 10:53:22 +0530

    style: implement smooth scrolling for hash links in HomePage component

--- client/src/features/public/pages/HomePage.tsx
+++ client/src/features/public/pages/HomePage.tsx
@@ -8,8 +8,21 @@ import WhyChooseUs from "../section/WhyChooseUs";
 import IntegrationSnippetsSection from "../section/Integrationsnippetssection";
 import FAQSection from "../section/FAQSection";
 import CTASection from "../section/CTASection";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      const timeout = setTimeout(() => {
+        document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+      return () => clearTimeout(timeout);
+    }
+  }, [location]);
   return (
     <div className="top-16">
       <HeroSection />

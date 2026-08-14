4391d110a57bfade9cab1d3e90013eb7730575c3
Ayush Karma <ayushkarma.dev@gmail.com>
2026-08-14 17:57:56 +0530

    style: hide HeroDashboardImage on smaller screens

--- client/src/features/public/section/HeroDashboardImage.tsx
+++ client/src/features/public/section/HeroDashboardImage.tsx
@@ -2,7 +2,7 @@ import HeroUserDashboardImage from "@/features/public/assets/hero-dashboard.png"
 
 function HeroDashboardImage() {
   return (
-    <div className="bg-gray-50 relative h-[90svh]">
+    <div className="bg-gray-50 relative h-[90svh] hidden md:block">
       <div className="absolute -top-20 left-0 right-0 h-32 z-20 backdrop-blur-md [mask-image:linear-gradient(to_top,black,transparent)] pointer-events-none" />
       <div className="absolute -top-20 left-0 right-0 h-40 z-20 bg-gradient-to-t from-gray-50 via-gray-50/60 to-transparent pointer-events-none" />
       <div className="max-w-4/5 h-[95svh] flex mx-auto relative">

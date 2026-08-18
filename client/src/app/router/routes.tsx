eaa9b3b4fe3aa3a6b1fa86e0bd8ecc73f179fa47
Ayush Karma <ayushkarma.dev@gmail.com>
2026-08-18 16:27:02 +0530

    fix: remove vercel analytics

--- client/src/app/router/routes.tsx
+++ client/src/app/router/routes.tsx
@@ -25,10 +25,8 @@ import AppSettings from "@/features/settings/pages/AppSettings";
 import NotFound from "@/features/public/pages/NotFound";
 import LearnMorePage from "@/features/public/pages/LearnMorePage";
 import PrivacyPolicyPage from "@/features/public/pages/PrivacyPolicyPage";
-import { Analytics } from "@vercel/analytics/react";
 export const appRoutes = (
   <Routes>
-    <Analytics />
     <Route element={<AuthLayout />}>
       <Route path="/login" element={<LoginPage />} />
 
